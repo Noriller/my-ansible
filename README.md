10a1636fd9a60da27368e6d25993f177b2c0f248
Bruno Noriller <brunonoriller@gmail.com>
2022-02-14 15:14:31 -0300

    initial
    links
    
    links
    
    wip
    
    link
    
    adding flag
    
    ident
    
    update after add repo
    
    trying changes
    
    test
    
    update
    
    changes
    
    sudoer
    
    shell
    
    test
    
    t
    
    x
    
    test
    
    t
    
    t
    
    x
    
    t
    
    s
    
    x
    
    x
    
    x
    
    x
    
    x
    
    s
    
    t
    
    sutff
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    xx
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    xx
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    xx
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    gno
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    sd
    
    z
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    x
    
    adding code as default git editor

---
+++ README.md
@@ -0,0 +1,61 @@
+# My Ansible Playbook
+
+This is my ansible solution to setup and configure a new computer.
+
+## Start
+
+I start with a Ubuntu 20.04 LTS "minimal" install.
+
+Then:
+```bash
+sudo bash -c "$(wget -O - https://raw.github.com/Noriller/my-ansible/master/start.sh)"
+```
+
+Reboot after finishing.
+
+## Things you might want to do before starting:
+
+### Healthchecks (optional)
+
+Create an account and have a UUID ready for it.
+
+Create the account here:
+
+[https://healthchecks.io/](https://healthchecks.io/)
+
+
+This will send an email notification in case of failures.
+
+### Telegram (optional)
+
+Setup a telegram bot and have the chat_id and token ready.
+
+This will send notifications in case of success or failure.
+
+## Should I use this as is?
+
+Probably not.
+
+These are my options, and since I'm new to Linux in general, I believe I gonna change a lot of things to fit my needs.
+
+Probably in a daily basis!
+
+### What you can do is:
+
+- Fire a VM, make a fresh install, and then run this.
+- See if you like or not the results.
+- Fork and change stuff as needed.
+- ???
+- Profit.
+
+Also:
+
+- do a `dconf dump / > your-dump.txt` on your Ubuntu
+- do a `dconf dump / > my-dump.txt` on the VM after everything is installed
+- `diff your-dump.txt my-dump.txt` to see the differences (I actually do this on VSCode and it's pretty easy to do)
+
+Use that as a reference to see what you would need to change.
+
+---
+
+And hit me up if you have any questions, suggestions, or just want to say hi.
---
+++ start.sh
@@ -0,0 +1,84 @@
+#!bin/bash
+
+if [ "$EUID" -ne 0 ]
+  then echo "Please run as root / sudo"
+  exit
+fi
+
+echo -e "\n"
+echo "Enter the following information:"
+
+echo -e "\n"
+echo "Enter your username:"
+read -e -p "Enter Ubuntu username: ($SUDO_USER) " -i $SUDO_USER user_login
+
+echo -e "\n"
+echo "Let's set the GIT global credentials (optional):"
+read -e -p "Enter Git Name (optional): " git_user_name
+read -e -p "Enter Git Email (optional): " git_user_email
+
+# TODO: check the telegram api, apparently things changed
+# echo -e "\n"
+# echo "Let's set the telegram chat to check on the ansible jobs (optional):"
+# echo "This will make sure that the cron job will send a message to the telegram chat on success or failure"
+# read -e -p "Enter Telegram Chat ID (optional): " telegram_chat_id
+# read -e -p "Enter Telegram Token (optional): " telegram_token
+
+echo -e "\n"
+echo "Let's set the Healthchecks (https://healthchecks.io/) to additionally check on the ansible jobs (optional):"
+echo "Get the UUID of the Healthchecks:"
+read -e -p "Enter Healthcheck UUID (optional): " healthcheck_uuid
+
+echo -e "\n\n\n"
+echo "Confirm the information:"
+echo "Username: $user_login"
+echo "Git Name: $git_user_name"
+echo "Git Email: $git_user_email"
+echo "Telegram Chat ID: $telegram_chat_id"
+echo "Telegram Token: $telegram_token"
+echo "Healthcheck UUID: $healthcheck_uuid"
+
+read -e -p "Confirm? [Y/n] " -i 'Y' response
+
+while true; do
+  case "$response" in
+      [yY][eE][sS]|[yY])
+        echo "Starting the installation..."
+        break
+        ;;
+      *)
+        echo "Aborting..."
+        exit
+        ;;
+  esac
+done
+
+
+# add repositories
+sudo apt-add-repository ppa:ansible/ansible
+
+# updates the system
+sudo apt update
+sudo apt upgrade -y
+
+# install ansible and git
+sudo apt install ansible git -y
+
+# ansible pull the playbook
+ansible-pull -U 'https://github.com/Noriller/my-ansible.git' \
+  -e "user_login=$user_login" \
+  -e "cron_job_name='first install'" \
+  -e "git_user_name=$git_user_name" \
+  -e "git_user_email=$git_user_email" \
+  -e "telegram_chat_id=$telegram_chat_id" \
+  -e "telegram_token=$telegram_token" \
+  -e "healthcheck_uuid=$healthcheck_uuid" \
+
+# use --tags "tag1,tag2" to run only the tasks tagged with tag1 and tag2
+# use this to limit stuff instead of hosts in the playbook (since we don't know the hosts yet using pull)
+# use "tags: always" to set up to always run the play
+
+# use --tags "always" to run only the tasks tagged with "always" (basically the basic stuff)
+# run without --tags to run everything
+
+# use --check --diff to make a dry run and see what would change
